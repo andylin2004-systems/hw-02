fde3b2ff3ff65c1699bfaf9ad8678622a34c5cd0
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-09-19 09:10:18 -0400

    tried to fix issue where unsigned long did not respond correctly

--- ExperimentProgram.c
+++ ExperimentProgram.c
@@ -13,11 +13,11 @@ int main() {
     printf("Max unsigned long plus max long as an unsigned long equals %lu\n", unsignedMaxLongAndLongMaxAsUnsignedLong);
     printf("this aint cooked enough lets ramp this up a bit\n");
     printf("Max unsigned int plus max int plus nice number as an int equals %d\n", unsignedMaxIntAndIntMaxAsInt + 69);
-    unsigned int niceBoi = INT_MAX + UINT_MAX + 420;
+    unsigned int niceBoi = (INT_MAX * 2U / 2U) + UINT_MAX + 420U;
     unsignedMaxandIntMaxButUnsigned = niceBoi;
     printf("Max unsigned int plus max int plus another nice number as an unsigned int equals %i\n", unsignedMaxandIntMaxButUnsigned);
     printf("Max unsigned long plus max long plus the year we hated the most as an long equals %ld\n", unsignedMaxLongAndLongMaxAsLong + 2020);
-    unsignedMaxLongAndLongMaxAsUnsignedLong = LONG_MAX + ULONG_MAX + 7397l;
+    unsignedMaxLongAndLongMaxAsUnsignedLong = (LONG_MAX * 2UL / 2UL) + ULONG_MAX + 7397UL;
     printf("Max unsigned long plus max long plus my m1's geekbench score (7397) as an unsigned long equals %lu\n", unsignedMaxLongAndLongMaxAsUnsignedLong);
     printf("Pi to 3 sig figs is %0.3f\n", M_PI);
     printf("Pi to 4 sig figs is %0.4f\n", M_PI);
