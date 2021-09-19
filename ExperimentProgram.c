8f85b42dbfa5a3139274fb7dbfdd6e10f69b11fa
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-09-19 08:53:43 -0400

    attempting to expose more of the overflow but ints are a pain

--- ExperimentProgram.c
+++ ExperimentProgram.c
@@ -11,6 +11,14 @@ int main() {
     printf("Max unsigned int plus max int as an unsigned int equals %i\n", unsignedMaxandIntMaxButUnsigned);
     printf("Max unsigned long plus max long as an long equals %ld\n", unsignedMaxLongAndLongMaxAsLong);
     printf("Max unsigned long plus max long as an unsigned long equals %lu\n", unsignedMaxLongAndLongMaxAsUnsignedLong);
+    printf("this aint cooked enough lets ramp this up a bit\n");
+    printf("Max unsigned int plus max int plus nice number as an int equals %d\n", unsignedMaxIntAndIntMaxAsInt + 69);
+    unsigned int niceBoi = INT_MAX + UINT_MAX + 420;
+    unsignedMaxandIntMaxButUnsigned = niceBoi;
+    printf("Max unsigned int plus max int plus another nice number as an unsigned int equals %i\n", unsignedMaxandIntMaxButUnsigned);
+    printf("Max unsigned long plus max long plus the year we hated the most as an long equals %ld\n", unsignedMaxLongAndLongMaxAsLong + 2020);
+    unsignedMaxLongAndLongMaxAsUnsignedLong = LONG_MAX + ULONG_MAX + 7397l;
+    printf("Max unsigned long plus max long plus my m1's geekbench score (7397) as an unsigned long equals %lu\n", unsignedMaxLongAndLongMaxAsUnsignedLong);
     printf("Pi to 3 sig figs is %0.3f\n", M_PI);
     printf("Pi to 4 sig figs is %0.4f\n", M_PI);
     printf("Pi to 5 sig figs is %0.5f\n", M_PI);
